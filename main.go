6eab180077895a9ffe029ce69a85855d6326f94e
Dan <lienglex@gmail.com>
2021-04-04 22:47:05 +0300

    added: omit dirs functionality

--- main.go
+++ main.go
@@ -13,17 +13,27 @@ const OrigExt = ".orig"
 
 var _FILES = make(chan string, 20)
 
+const NodeModulesDirName = "node_modules"
+const RootFolderPath = "./"
+
 func main() {
-	var rootFlag = flag.String("root", ".", "relative root directory")
+	var rootFlag = flag.String("root", RootFolderPath, "relative root directory")
+	var omitDirsFlag = flag.String("omitDirs", NodeModulesDirName, "directories through comma, where files with `.orig` ext won't be removed\ne.g --omitDirs='node_modules,someOtherDir,AndSoOnDir'\n")
 
 	flag.Parse()
 
-	err := filepath.Walk(*rootFlag, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
+	omitDirs := strings.ReplaceAll(*omitDirsFlag, " ", "")
+
+	err := filepath.WalkDir(*rootFlag, func(path string, d os.DirEntry, err error) error {
+		if d.IsDir() {
+			for _, omitDirName := range strings.Split(omitDirs, ",") {
+				if d.Name() == omitDirName {
+					return filepath.SkipDir
+				}
+			}
 		}
 
-		if info.Mode().IsRegular() {
+		if d.Type().IsRegular() {
 			go writeFilePathWithExt(path, OrigExt)
 		}
 
